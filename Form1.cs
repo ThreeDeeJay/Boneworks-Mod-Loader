4c5633a044d26f6e43048cc36459a5b21af9fddd
cixxytt <barcode6657@gmail.com>
2022-01-22 10:34:57 +0000

    Added Panel Controls

--- Form1.cs
+++ Form1.cs
@@ -53,7 +53,57 @@ namespace Loader
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			userControl11.Hide();
+			userControl21.Show();
+			userControl31.Hide();
+			userControl41.Hide();
+		}
 
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void bunifuButton1_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void bunifuButton2_Click(object sender, EventArgs e)
+		{
+			userControl11.Hide();
+			userControl21.Show();
+			userControl31.Hide();
+			userControl41.Hide();
+		}
+
+		private void bunifuButton3_Click(object sender, EventArgs e)
+		{
+			userControl11.Show();
+			userControl21.Hide();
+			userControl31.Hide();
+			userControl41.Hide();
+		}
+
+		private void bunifuButton4_Click(object sender, EventArgs e)
+		{
+			userControl11.Hide();
+			userControl21.Hide();
+			userControl31.Show();
+			userControl41.Hide();
+		}
+
+		private void bunifuButton5_Click(object sender, EventArgs e)
+		{
+			userControl11.Hide();
+			userControl21.Hide();
+			userControl31.Hide();
+			userControl41.Show();
 		}
 	}
 }
